50b0e223d0987ca976a900758fddbd96896cba3a
Nozdrin Evgenii <ipnozdrin57@yandex.ru>
2023-02-28 08:22:59 +0300

    commit 2

--- home_work_open_files.py
+++ home_work_open_files.py
@@ -2,10 +2,7 @@ with open('recipes.txt', encoding='utf-8') as file:
     cook_book = {}
     for line in file:
         dish = line.strip()
-        # print(dish)
-        # print('---------')
         lin = int(file.readline())
-        # print(lin)
         ingredients = []
         for _ in range(int(lin)):
             emp = file.readline().strip()
@@ -23,11 +20,8 @@ def get_shop_list_by_dishes(dishs, person):
 
     for dish in dishs:
         if dish in cook_book:
-            for a in cook_book[dish]:
-                # print(a)
                 if a['ingredient_name'] not in nec_ing:
                     nec_ing[a['ingredient_name']] = {'measure': a['measure'], 'quantity': int(a['quantity']) * person}
-                    # print(nec_ing[a['ingredient_name']])
                 else:
                     nec_ing[a['ingredient_name']]['quantity'] += int(a['quantity']) * person
     print(nec_ing)
